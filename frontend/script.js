c0148a104443fee485f8c3b215596d84521afc22
dianaventura <d.ventura@rgu.ac.uk>
2024-02-19 11:28:29 +0000

    removed browser validation replaced with js validation for forms

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,40 @@
-//nothing yet :) 
+
+
+  document.getElementById('raffle-entry').addEventListener('submit', function(event) {
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    if(!name || !email){
+        alert('You need a name and an email to enter this raffle');
+        event.preventDefault();
+    }
+
+  });
+
+  
+
+  document.getElementById('login').addEventListener('submit', function(event) {
+
+    var email= document.getElementById('login-email').value;
+    var password = document.getElementById('login-password').value;
+
+    if(!email || !password){
+        alert('Please enter an email and a password to log in!');
+        event.preventDefault();
+    }
+
+  });
+
+  document.getElementById('signup').addEventListener('submit', function(event) {
+
+    var name = document.getElementById('signup-name').value;
+    var email= document.getElementById('signup-email').value;
+    var password = document.getElementById('signup-password').value;
+
+    if(!email || !password){
+        alert('Please enter an email, password and name to sign up !');
+        event.preventDefault();
+    }
+
+  });
