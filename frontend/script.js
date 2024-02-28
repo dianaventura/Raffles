ec2bcea3abb074bbcc74bc07b53bf87b6140bbd8
dianaventura <d.ventura@rgu.ac.uk>
2024-02-28 13:52:30 +0000

    post req for user info

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,4 @@
 
-const express = require('express');
-const app = express();
-const { MongoClient } = require("mongodb");
-const uri = "mongodb://127.0.0.1:27017";
-const port = 8080;
-
-app.use(express.json());
 
   document.getElementById('raffle-entry').addEventListener('submit', function(event) {
 
@@ -35,67 +28,49 @@ app.use(express.json());
 
   document.getElementById('signup').addEventListener('submit', function(event) {
 
+    event.preventDefault();
+
     var name = document.getElementById('signup-name').value;
     var email= document.getElementById('signup-email').value;
     var password = document.getElementById('signup-password').value;
 
-    if(!email || !password){
+
+
+
+    //sending name email and password to backend
+
+    if(!email || !password|| name){
         alert('Please enter an email, password and name to sign up !');
-        event.preventDefault();
-    }
+        
+    }else{
 
+      var createUser = {
+        name: name,
+        email: email,
+        password: password
+
+      
+    };
+
+    //post request
+
+    fetch('/createUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(createUser)
+  })
+  .then(response.json())
+  .then(data => {
+    console.log('Success:', data);
+
+  })
+  .catch((error)=> {
+    console.error('Error', error);
   });
 
-  //adapted from lab 4 
-  app.post('/api/databaseAction', async (req, res) => {
-  // Database stuff
-    // Create a new MongoClient
-    const client = new MongoClient(uri);
-    async function run() {
-    try {
-        // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
-        // Establish and verify connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Connected successfully to server");
-        console.log('Start the database stuff');
-        //Write databse Insert/Update/Query code here..
- console.log('End the database stuff');
+}
 
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-    }
-    run().catch(console.dir);
-  });
-
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
 });
 
-//adapted from lab 4 
-
-app.post('/createUser', async (req, res) => {
-  // Create a new MongoClient
-  const client = new MongoClient(uri);
-  const { username, email, password } = req.body; // extract user info from request body
-
-  try {
-      await client.connect();
-      var dbo = client.db("mydb");
-      var myobj = { username, email, password }; 
-      await dbo.collection("users").insertOne(myobj,function(err,res){
-
-      if(err){
-        console.log(err);
-        throw err;
-      }
-      console.log('USER CREATED');
-    });
-    console.log('End the database stuff')
- 
-  } finally {
-      await client.close();
-  }
-});
