e6cc5e5dcdec41eda10ef303c6ab2e3bab6fc352
dianaventura <d.ventura@rgu.ac.uk>
2024-02-29 13:04:20 +0000

    api bug fixed

--- simpleServer.js
+++ simpleServer.js
@@ -13,12 +13,6 @@ app.use(express.json());
 //serve static files like css etc
 app.use(express.static(path.join(__dirname,'frontend')));
 
-// Handling 404 errors
-app.use((req, res, next) => {
-  console.log('Requested URL:', req.url);
-  res.status(404).send("test A");
-});
-
 
 
 
@@ -78,4 +72,15 @@ app.use((req, res, next) => {
 // Start the server
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+});
+
+// Handling 404 errors
+app.use((req, res, next) => {
+  console.log('Requested URL:', req.url);
+  res.status(404).send("test A");
+});
+// Handling 404 errors
+app.use((req, res, next) => {
+  console.log('Requested URL:', req.url);
+  res.status(404).send("test A");
 });
