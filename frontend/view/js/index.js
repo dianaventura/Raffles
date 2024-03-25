8a544f771cf09c22c873a53af2770ebff39e6e8b
dianaventura <d.ventura@rgu.ac.uk>
2024-03-25 15:50:38 +0000

    Added form to json handling for guest entry

---
+++ frontend/view/js/index.js
@@ -0,0 +1,47 @@
+//handling html makeing fetch
+
+document.getElementById('raffle-entry').addEventListener('submit', function(event) {
+ 
+    event.preventDefault();
+
+    var name = document.getElementById('guest-name').value;
+    var email = document.getElementById('guest-email').value;
+ 
+
+
+    //using input to 
+
+    if(email.length == 0|| name.length == 0){
+        alert('Please enter an email, password and name to sign up !');
+        
+    }else{
+        console.log('Adding user info ')
+      var guestData = {
+        name: name,
+        email: email,
+      
+    };
+
+    //post request to server side 
+
+    fetch('/enter-as-guest', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(guestData)
+  })
+  .then(response => response.json() )
+  .then(data => {
+    console.log('Success:', data);
+    alert('You have been entered into the raffle!')
+
+  })
+  .catch((error)=> {
+    console.error('Error', error);
+    alert('An error occured. Please try again!!! ')
+  });
+
+}
+
+});
