20caf09a8b846371f3353ac82b51e9e6a10682a3
dianaventura <d.ventura@rgu.ac.uk>
2024-03-26 14:38:37 +0000

    Fixed path in raffleRoute

---
+++ controllers/raffleController.js
@@ -0,0 +1,16 @@
+// raffle controller
+
+const {MongoClient} = require("mongodb");
+const uri = process.env.DB_URI;
+const Raffle = require('../models/Raffle')
+
+exports.getRaffles = async(req,res) => {
+    const{name, email} = req.body;
+    try{
+        
+        const raffles = await Raffle.find(); //fetch all raffles from backend
+        res.status(201).json(raffles)
+    }catch(error){
+        res.status(500).json({error: 'Could not find raffles :('})
+    }
+}
