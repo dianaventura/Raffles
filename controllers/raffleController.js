18a07d0c5401ae667c1c35b18e060fe0c97a8a20
dianaventura <d.ventura@rgu.ac.uk>
2024-03-26 19:55:19 +0000

    Added cors to server set up

--- controllers/raffleController.js
+++ controllers/raffleController.js
@@ -1,14 +1,12 @@
 // raffle controller
 
-const {MongoClient} = require("mongodb");
-const uri = process.env.DB_URI;
 const Raffle = require('../models/Raffle')
 
 exports.getRaffles = async(req,res) => {
     try{
         
         const raffles = await Raffle.find(); //fetch all raffles from backend
-        res.status(201).json(raffles)
+        res.status(200).json(raffles)
     }catch(error){
         res.status(500).json({error: 'Could not find raffles :('})
     }
