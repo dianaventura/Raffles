09cee705573d747b8bc2e19016f88c52ea64ec1a
dianaventura <d.ventura@rgu.ac.uk>
2024-03-26 15:24:51 +0000

    console logs for debugging

--- frontend/view/js/index.js
+++ frontend/view/js/index.js
@@ -1,5 +1,40 @@
+
+function fetchRaffles(){
+    console.log('Fetching raffles...'); // Log before fetch
+    fetch('/get-raffles')
+    
+        .then(response => response.json())
+        .then(raffles => displayRaffles(raffles))
+        .catch(error => console.error('Error fetching raffles', error));
+}
+
+function displayRaffles(raffles) {
+    //get raffle container
+
+    const container = document.getElementById('raffle-container');
+    //clear
+    container.innerHTML = ''; 
+
+    raffles.forEach(raffle => {
+        const raffleHTML = `
+            <div class="raffle-card">
+
+                <img src="${raffle.imageUrl}" alt="Raffle" class="raffle-image">
+                <h3>${raffle.title}</h3>
+                <p>Prize: ${raffle.prize}</p>
+                <p>Draw Date: ${raffle.drawDate}</p>
+                <button onclick="enterRaffleAsGuest('${raffle.id}')">Enter This Raffle</button>
+            </div>
+        `;
+        container.innerHTML += raffleHTML;
+    });
+}
+
+
 //handling html makeing fetch
-document.addEventListener('DOMContentLoaded', (event) => {
+
+
+
 document.getElementById('raffle-entry').addEventListener('submit', function(event) {
  
     event.preventDefault();
@@ -46,4 +81,45 @@ document.getElementById('raffle-entry').addEventListener('submit', function(even
 
 });
 
-});
+// this is called from rafflecards
+function enterRaffleAsGuest(raffleId) {
+
+    const name = prompt("Please enter your name:");
+    const email = prompt("Please enter your email:");
+
+    if (!email || !name) {
+        alert('Please enter an email and name to sign up!');
+        return;
+    }
+
+    const guestData = {
+        name: name,
+        email: email,
+        raffleId: raffleId 
+    };
+
+    fetch('/enter-as-guest', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(guestData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        alert('You have been entered into the raffle!');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again!');
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    fetchRaffles(); //fetches and displayes raffles 
+    
+    });
+    
