6c5e0d09b7bf81c933fb184bf0a60b0e9155bb0f
dianaventura <d.ventura@rgu.ac.uk>
2024-03-26 14:19:14 +0000

    Adding changes to Guest Model to take raffleId

--- models/Guest.js
+++ models/Guest.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const guestEntry = new mongoose.Schema({
     name: String,
     email: String,
+    raffleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Raffle'},
 });
 
 const Guest = mongoose.model('Guest', guestEntry);
