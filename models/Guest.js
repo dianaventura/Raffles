d4e6bd438cf880368842c2b4c3c0b87ddbc6d216
dianaventura <d.ventura@rgu.ac.uk>
2024-03-28 15:51:21 +0000

    Added hook to generate token for guest entry

--- models/Guest.js
+++ models/Guest.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
+
+const crypto = require('crypto');
+
 const guestEntry = new mongoose.Schema({
     name: String,
     email: String,
     raffleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Raffle'},
+    token: {type: String, unique: true}
 });
 
 const Guest = mongoose.model('Guest', guestEntry);
+
+//hook to generate token when a guest is entered.
+guestEntry.pre('save', function(next) {
+
+    if (!this.token) {
+
+        this.token = crypto.randomBytes(16).toString('hex');
+
+    }
+
+    next();
+});
 
 module.exports = Guest;
