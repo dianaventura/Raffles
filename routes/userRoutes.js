73a1b24508f805c3808b9adc6adc7029336eee5b
dianaventura <d.ventura@rgu.ac.uk>
2024-03-28 17:18:04 +0000

    Fixed typo in user controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const guestController = require('../controllers/guestController');
+const guestController = require('../controllers/userController');
 
 // Route for user entry
 router.post('/signup', userController.signup);
